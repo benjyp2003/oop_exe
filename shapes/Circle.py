8500bf01ac3e1b7b070b9cf8bd8bf1c4329501b0
benjyp2003 <benjypfeffer@gmail.com>
2025-06-25 14:40:10 +0300

    saved circle changes

--- shapes/Circle.py
+++ shapes/Circle.py
@@ -5,9 +5,10 @@ class Circle(Shape):
 
     def __init__(self, width):
         super().__init__(width, width)
+        self.radius = self.width / 2
 
     def get_area(self):
-        return math.pi * (self.width / 2) ** 2
+        return math.pi * self.radius ** 2
 
     def get_perimeter(self):
-        return 2 * math.pi * (self.width / 2)
+        return 2 * math.pi * self.radius
